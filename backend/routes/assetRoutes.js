8e03dd0054f7c2b6a4c462051bc0bc0c9d4e3c41
sudesh-bhattarai <bhattaraisudesh08@gmail.com>
2026-07-21 19:08:19 +0545

    Assets routes added

--- backend/routes/assetRoutes.js
+++ backend/routes/assetRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const controller = require('../controllers/assetController');
 
+const router = express.Router();
+
+router.get('/', controller.getAssets);
+router.get('/:id', controller.getAssetById);
+router.post('/', controller.createAsset);
+router.put('/:id', controller.updateAsset);
+router.delete('/:id', controller.deleteAsset);
+
+module.exports = router;
