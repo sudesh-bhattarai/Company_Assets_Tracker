ccaccfa99caaac81847dbb3a03bb0c8dd5074269
sudesh-bhattarai <bhattaraisudesh08@gmail.com>
2026-07-25 22:06:25 +0545

    Employee routes

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const controller = require('../controllers/employeeController');
 
+const router = express.Router();
+
+router.get('/', controller.getEmployees);
+router.get('/:id', controller.getEmployeeById);
+router.post('/', controller.createEmployee);
+router.put('/:id', controller.updateEmployee);
+router.delete('/:id', controller.deleteEmployee);
+
+module.exports = router;
